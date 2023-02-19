58a8ea3ca59a9a5a1152260db66c86d56b3312c1
WillardIV <RobinsonWM17@gmail.com>
2023-02-19 18:49:16 -0500

    reset from large file problem

--- code/open-pickle-datasets.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-def my_open(file, comp='zip'):
-    df = pd.read_pickle(file + '.pkl.zip', compression = comp)
-    df.to_csv(file + '.csv')
-
-# my_open('curated_datasets/Sector_Addresses')
-# my_open('curated_datasets/RouteQualityData')
-# my_open('curated_datasets/All_Ratings')
